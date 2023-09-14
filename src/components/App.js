1ff50da71228ab6d169c2a5c4fe44b95c8c0d97c
VIIBII <birlavishal90@gmail.com>
2023-09-15 00:27:41 +0530

    submitting code-Fri Sep 15 2023 00:27:40 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,93 @@
-import React from "react";
-import "./../styles/App.css";
+import React, { useState } from "react";
+import "./app.css";
 
-const App = () => {
-  return <div>Hello</div>;
-};
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Submit the form
+  };
+
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setName(value);
+    if (!value) {
+      setNameError("Name is required");
+    } else {
+      setNameError("");
+    }
+  };
+
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+    if (!value) {
+      setEmailError("Email is required");
+    } else if (!validateEmail(value)) {
+      setEmailError("Invalid email format");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const handlePasswordChange = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+    if (!value) {
+      setPasswordError("Password is required");
+    } else if (value.length < 6) {
+      setPasswordError("Password must be at least 6 characters");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={name}
+        onChange={handleNameChange}
+      />
+      {nameError && <div className="error-message">{nameError}</div>}
+
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      {emailError && <div className="error-message">{emailError}</div>}
+
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      {passwordError && <div className="error-message">{passwordError}</div>}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 export default App;
